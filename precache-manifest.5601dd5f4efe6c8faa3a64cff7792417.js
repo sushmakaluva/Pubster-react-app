8913a4929876dbeef0584c7a549931a697a6fbdc
Sushma Kaluva <kaluvasushma@gmail.com>
2020-10-13 17:38:34 -0400

    Updates

---
+++ precache-manifest.5601dd5f4efe6c8faa3a64cff7792417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a21d1c8bf3eba9ad5fd0acb8bae7a25",
+    "url": "/pubster-react-app/index.html"
+  },
+  {
+    "revision": "ec5146879fae0e2d8331",
+    "url": "/pubster-react-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0d168dd54f7165e78081",
+    "url": "/pubster-react-app/static/css/main.718ea7af.chunk.css"
+  },
+  {
+    "revision": "ec5146879fae0e2d8331",
+    "url": "/pubster-react-app/static/js/2.a059c1cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pubster-react-app/static/js/2.a059c1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d168dd54f7165e78081",
+    "url": "/pubster-react-app/static/js/main.0ef84fd6.chunk.js"
+  },
+  {
+    "revision": "8bf36a58cfc7dc7712b5",
+    "url": "/pubster-react-app/static/js/runtime-main.b61a5f68.js"
+  }
+]);
